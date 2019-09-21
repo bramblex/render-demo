5989180bd50e3ccce40437fd49a393731acd65ef
乔健 <qjnight@gmail.com>
2019-09-21 08:26:15 +0800

    feat: clear cavas before render

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 document.getElementById('render').addEventListener('click', function () {
+    document.getElementById('output-canvas').getContext('2d').clear()
     render(
         document.getElementById('input-html').value,
         document.getElementById('input-css').value
